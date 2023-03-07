2cad4b91be51f1a5df45e1d99c3b5e1d6943a3c9
Kharina Irina <dubmira88@gmail.com>
2023-03-07 09:42:01 +0500

    Example003 Sum project

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.Write("3 + 5 = ");
+Console.WriteLine(numberA + numberB);
+Console.Write("3 + 5 = ");
+Console.WriteLine(result);
