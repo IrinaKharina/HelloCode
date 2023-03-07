cf0db91a01baeb307b976403b243298ab4c788c2
Kharina Irina <dubmira88@gmail.com>
2023-03-07 17:38:11 +0500

    Add Random elements

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -5,3 +5,11 @@ Console.Write("3 + 5 = ");
 Console.WriteLine(numberA + numberB);
 Console.Write("3 + 5 = ");
 Console.WriteLine(result);
+
+int numberC = new Random().Next(1,10); // 1 2 3 4 ... 9
+int numberD = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.Write(numberC);
+Console.Write(" + ");
+Console.Write(numberD);
+Console.Write(" = ");
+Console.WriteLine(numberC + numberD);
