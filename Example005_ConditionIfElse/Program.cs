27a4dd731927703fb5fa532c3a67d52499c29a98
Kharina Irina <dubmira88@gmail.com>
2023-03-12 13:28:31 +0500

    Examle005 Condition If Else project

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") //перевод символов в нижний регистр
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+    //Console.Write("Привет, ", username);
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
